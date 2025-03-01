1e51b863ff675772d79025382232971d05299b9d
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-03-01 21:44:23 +0500

    Add gemfile to project cor logic laid out
    gemfile to project add all logic for extracting zipcode and names laid out

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,21 @@
-puts "Event Manager Initialized!"
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
