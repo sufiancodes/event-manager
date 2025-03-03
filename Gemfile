7ef165dc892e6786fc22557ee42a6ec42c8ccadf
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-03-03 13:07:26 +0500

    Add method for number one gem add
    method to clean and retrieve the phonenumbers add along with gem for debugging

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "google-apis-civicinfo_v2", "~> 0.24.0"
+
+gem "pry-byebug", "~> 3.10"
--- lib/assignment.rb
+++ lib/assignment.rb
@@ -7,12 +7,29 @@ content = CSV.open(
   header_converters: :symbol
 )
 
-def get_phone_number(number)
-  number = number.to_s
+def clean_phone_number(number)
+  number = number.to_s.tr('^0-9', '')
   if number.length < 10
-    number = 0000000000
+    number = "0000000000"
+  elsif number.length == 10
+    number
+  elsif number.length == 11
+    if number.chars[0] == 1
+      number.delete_prefix('1')
+    else
+      number = "0000000000"
+    end
+  elsif number.length > 11
+    number = "0000000000"
   end
+
 end
+
+puts "Here is name and number"
 content.each do |row|
-  puts row[:homephone]
+  id = row[0]
+  name = row[:first_name] + " " + row[:last_name]
+  phone = clean_phone_number(row[:homephone])
+  puts "#{name} #{phone}"
 end
+
