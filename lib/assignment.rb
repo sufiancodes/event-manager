d2eef8bb1e670602245234973424594cdca6f8ce
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-03-04 22:02:47 +0500

    Rubocop install and use
    rubocop install and use on the project to improve code

--- lib/assignment.rb
+++ lib/assignment.rb
@@ -1,43 +1,45 @@
-require 'time'
-require 'date'
-require 'csv'
-require 'erb'
+# frozen_string_literal: true
+
+require "time"
+require "date"
+require "csv"
+require "erb"
 
 # opening the CSV file for reading
 content = CSV.open(
-  'event_attendees.csv',
+  "event_attendees.csv",
   headers: true,
-  header_converters: :symbol
+  header_converters: :symbol,
 )
 
 # method to clean phone numbers
 def clean_phone_number(number)
-  number = number.to_s.tr('^0-9', '')
+  number = number.to_s.tr("^0-9", "")
   if number.length < 10
-    number = "0000000000"
+    "0000000000"
   elsif number.length == 10
     number
   elsif number.length == 11
     if number.chars[0] == 1
-      number.delete_prefix('1')
+      number.delete_prefix("1")
     else
-      number = "0000000000"
+      "0000000000"
     end
   elsif number.length > 11
-    number = "0000000000"
+    "0000000000"
   end
 end
 
 # method to convert date to day
 def date_to_day(datetime)
-  date_object = DateTime.strptime(datetime, '%m/%d/%Y %k : %M')
-  day = date_object.wday
+  date_object = DateTime.strptime(datetime, "%m/%d/%Y %k : %M")
+  date_object.wday
 end
 
 # method to convert date to hour
 def date_to_hour(datetime)
-  date_object = DateTime.strptime(datetime, '%m/%d/%Y %k : %M')
-  hour = date_object.hour
+  date_object = DateTime.strptime(datetime, "%m/%d/%Y %k : %M")
+  date_object.hour
 end
 
 # this block find the most important weekdays
@@ -46,8 +48,10 @@ puts "Here most important weekdays"
 content.each do |row|
   important_day = date_to_day(row[:regdate])
   popular_week_day = popular_week_day.push(important_day)
-  weekdays = popular_week_day.inject(Hash.new(0)) { |hash, value| hash[value] += 1 ; hash}
-  important_days = popular_week_day.max_by {|value| weekdays[value]}
+  weekdays = popular_week_day.each_with_object(Hash.new(0)) do |value, hash|
+    hash[value] += 1
+  end
+  important_days = popular_week_day.max_by { |value| weekdays[value] }
   # days start with Sunday and its index is 0
   puts important_days
 end
@@ -58,19 +62,18 @@ puts "Here are most important hour"
 content.each do |row|
   hour = date_to_hour(row[:regdate])
   popular_time_array = popular_time_array.push(hour)
-  hours = popular_time_array.inject(Hash.new(0)) {|hash, value| hash[value] += 1 ; hash}
-  peak_hour = popular_time_array.max_by {|value| hours[value]}
+  hours = popular_time_array.each_with_object(Hash.new(0)) do |value, hash|
+    hash[value] += 1
+  end
+  peak_hour = popular_time_array.max_by { |value| hours[value] }
   puts peak_hour
 end
 
-#this block puts the name and zipcode of people 
+# this block puts the name and zipcode of people
 puts "Here is name and number"
 content.each do |row|
-  id = row[0]
+  row[0]
   name = row[:first_name] + " " + row[:last_name]
   phone = clean_phone_number(row[:homephone])
   puts "#{name} #{phone}"
 end
-
-
-
--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,51 +1,52 @@
-require 'csv'
-require 'google/apis/civicinfo_v2'
-require 'erb'
+# frozen_string_literal: true
+
+require "csv"
+require "google/apis/civicinfo_v2"
+require "erb"
 
 # cleaning zipcode for use
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, "0")[0..4]
 end
-
 
 # sorting zipcode by legislator
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
   # handling the case where the info is not available
   begin
     civic_info.representative_info_by_address(
       address: zip,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      levels: "country",
+      roles: ["legislatorUpperBody", "legislatorLowerBody"],
     ).officials
   rescue
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
 end
 
 # creating and saving a thankyou letter
-def save_thank_you_letter(id,form_letter)
-  Dir.mkdir('output') unless Dir.exist?('output')
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exist?("output")
 
   filename = "output/thanks_#{id}.html"
 
-  File.open(filename, 'w') do |file|
+  File.open(filename, "w") do |file|
     file.puts form_letter
   end
 end
 
-puts 'EventManager initialized.'
+puts "EventManager initialized."
 # opening the CSV file
 contents = CSV.open(
-  'event_attendees.csv',
+  "event_attendees.csv",
   headers: true,
-  header_converters: :symbol
+  header_converters: :symbol,
 )
 
 # generating templates
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new(template_letter)
 
 # this block generate name and zipcode and save them in output folder
 contents.each do |row|
@@ -53,8 +54,8 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-  
+
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+  save_thank_you_letter(id, form_letter)
 end
